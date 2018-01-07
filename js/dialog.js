9b5f03653294b64023f2a99ab24edb9fb0cbd122
Alexandr Kokorev <ak_78leon@mail.ru>
2018-01-07 23:39:59 +0600

    добавляет dnd

--- js/dialog.js
+++ js/dialog.js
@@ -1,7 +1,7 @@
 // dialog.js
 'use strict';
 
-(function () {
+// (function () {
   var userDialog = document.querySelector('.setup');
   var userDialogOpen = document.querySelector('.setup-open');
   var userDialogClose = document.querySelector('.setup-close');
@@ -10,15 +10,28 @@
   var wizardEyes = userDialog.querySelector('.setup-wizard .wizard-eyes');
   var fireballWrap = userDialog.querySelector('.setup-fireball-wrap');
 
-  userDialog.classList.remove('hidden');
   userDialog.querySelector('.setup-similar').classList.remove('hidden');
+
+  var resetPosition = function (element) {
+    element.style.left = '';
+    element.style.top = '';
+  };
 
   var openPopup = function () {
     userDialog.classList.remove('hidden');
   };
 
+  openPopup();
+
+  document.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 27 && inputName !== document.activeElement) {
+      closePopup();
+    }
+  });
+
   var closePopup = function () {
     userDialog.classList.add('hidden');
+    resetPosition(userDialog);
   };
 
   userDialogOpen.addEventListener('click', function () {
@@ -47,6 +60,15 @@
     }
   });
 
+  var getCoords = function (elem) {
+    var box = elem.getBoundingClientRect();
+
+    return {
+      top: box.top + pageYOffset,
+      left: box.left + pageXOffset
+    };
+  };
+
   // Цвет мантии .setup-wizard .wizard-coat должен обновляться по нажатию на нее.
   //  Цвет мантии задается через изменение инлайнового CSS-свойства fill для элемента.
   // Цвет должен сменяться произвольным образом на один из следующих цветов:
@@ -63,8 +85,6 @@
     this.style.backgroundColor = window.util.generateRandomELement(window.constants.FIREBALL_COLORS);
   };
 
-
-
   // var changeWizardPartColor = function (wizardPart, color, propery) {
   //   var changeColor = function () {
   //     this.style.fill = window.util.generateRandomELement(color);
@@ -79,5 +99,131 @@
   wizardEyes.addEventListener('click', changeEyesColor);
   fireballWrap.addEventListener('click', changeFireballColor);
 
-})();
+  var dialogHandle = userDialog.querySelector('.setup-user-pic');
 
+  dialogHandle.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      var newY = userDialog.offsetTop - shift.y;
+      var newX = userDialog.offsetLeft - shift.x;
+      // обработаем вынос за верхнюю границу окна
+      if (newY < 0) {
+        newY = 0;
+      };
+      // // обработаем вынос за нижнюю границу окна
+      // if (newY + userDialog.offsetHeight > document.documentElement.clientHeight) {
+      //   newY = document.documentElement.clientHeight - userDialog.offsetHeight;
+
+      // };
+      // зажать в границах экрана по горизонтали
+      if (newX < 0) {
+        newX = 0;
+      }
+      // запара! translateX(-50%) сбивает выравнивание по горизонтали???
+
+      if (newX > document.documentElement.clientWidth - userDialog.offsetWidth) {
+        newX = document.documentElement.clientWidth  - userDialog.offsetWidth;
+      }
+
+
+      userDialog.style.top = newY + 'px';
+      userDialog.style.left = newX + 'px';
+      console.log(userDialog.style.left);
+      console.log(getCoords(userDialog).left);
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+
+// })();
+
+// Предметы, лежащие внутри блоков .setup-artifacts-cell и .setup-artifacts-shop,
+//  должны быть помечены как «передвигаемые» (draggable="true")
+
+
+
+var shopElement = document.querySelector('.setup-artifacts-shop');
+var draggedItem = null;
+
+var handleDragStart = function (evt) {
+  if (evt.target.tagName.toLowerCase() === 'img') {
+    draggedItem = evt.target;
+    evt.dataTransfer.setData('text/plain', evt.target.alt);
+  }
+};
+
+var artifactsElement = document.querySelector('.setup-artifacts');
+
+var handleDragOver = function (evt) {
+  evt.preventDefault();
+  return false;
+};
+
+var handleDragEnter = function (evt) {
+  evt.target.classList.add('over');
+  evt.preventDefault();
+};
+
+var handleDragLeave = function (evt) {
+  evt.target.classList.remove('over');
+  evt.preventDefault();
+};
+
+var handleDrop = function (evt) {
+  // if (draggedItem != this) {
+  //     draggedItem.innerHTML = this.innerHTML;
+  //     this.innerHTML = evt.dataTransfer.getData('text/html');
+  // }
+  evt.target.classList.remove('over');
+  evt.target.appendChild(draggedItem);
+  evt.preventDefault();
+};
+
+shopElement.addEventListener('dragstart', handleDragStart);
+artifactsElement.addEventListener('dragenter', handleDragEnter);
+artifactsElement.addEventListener('dragover', handleDragOver);
+artifactsElement.addEventListener('dragleave', handleDragLeave);
+artifactsElement.addEventListener('drop', handleDrop);
+
+
+
+
+
+
+
+// Зона в которую можно перетаскивать элемент должна получать красную рамку outline: 2px dashed red;
+//  во время начала движении и убираться при окончании
+// При передвижении элемента зона .setup-artifacts-cell над которой сейчас находится элемент
+//  должна подсвечиваться жёлтым (yellow)
+// При отпускании элемента в правильном месте, элемент должен оказываться на новой позиции,
+//  в противном случае все элементы оказываются на своих исходных местах
+// Дополнительно: Реализовать механизм не перетаскивания исходного элемента, а копирования.
+// При этом нужно запретить перетаскивать два элемента в одно и то же место
--- js/setup.js
+++ js/setup.js
@@ -21,7 +21,6 @@
 
   var renderWizard = function (wizard) {
     var wizardElement = similarWizardTemplate.cloneNode(true);
-    debugger;
     wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
     wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
     wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
